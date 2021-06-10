d232704d1fac362b5669307555c0661531408ab5
Amory Galili <amorygalili@gmail.com>
2021-06-10 15:09:54 -0400

    Added Chat.g4 and script to generate lexer and parser. Created basic html page

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # antlr4-example
+
+Antrl Installation
+==================
+
+https://github.com/antlr/antlr4/blob/master/doc/getting-started.md
---
+++ src/ChatListener.js
@@ -0,0 +1,90 @@
+// Generated from src/Chat.g4 by ANTLR 4.9.2
+// jshint ignore: start
+import antlr4 from 'antlr4';
+
+// This class defines a complete listener for a parse tree produced by ChatParser.
+export default class ChatListener extends antlr4.tree.ParseTreeListener {
+
+	// Enter a parse tree produced by ChatParser#chat.
+	enterChat(ctx) {
+	}
+
+	// Exit a parse tree produced by ChatParser#chat.
+	exitChat(ctx) {
+	}
+
+
+	// Enter a parse tree produced by ChatParser#line.
+	enterLine(ctx) {
+	}
+
+	// Exit a parse tree produced by ChatParser#line.
+	exitLine(ctx) {
+	}
+
+
+	// Enter a parse tree produced by ChatParser#name.
+	enterName(ctx) {
+	}
+
+	// Exit a parse tree produced by ChatParser#name.
+	exitName(ctx) {
+	}
+
+
+	// Enter a parse tree produced by ChatParser#command.
+	enterCommand(ctx) {
+	}
+
+	// Exit a parse tree produced by ChatParser#command.
+	exitCommand(ctx) {
+	}
+
+
+	// Enter a parse tree produced by ChatParser#message.
+	enterMessage(ctx) {
+	}
+
+	// Exit a parse tree produced by ChatParser#message.
+	exitMessage(ctx) {
+	}
+
+
+	// Enter a parse tree produced by ChatParser#emoticon.
+	enterEmoticon(ctx) {
+	}
+
+	// Exit a parse tree produced by ChatParser#emoticon.
+	exitEmoticon(ctx) {
+	}
+
+
+	// Enter a parse tree produced by ChatParser#link.
+	enterLink(ctx) {
+	}
+
+	// Exit a parse tree produced by ChatParser#link.
+	exitLink(ctx) {
+	}
+
+
+	// Enter a parse tree produced by ChatParser#color.
+	enterColor(ctx) {
+	}
+
+	// Exit a parse tree produced by ChatParser#color.
+	exitColor(ctx) {
+	}
+
+
+	// Enter a parse tree produced by ChatParser#mention.
+	enterMention(ctx) {
+	}
+
+	// Exit a parse tree produced by ChatParser#mention.
+	exitMention(ctx) {
+	}
+
+
+
+}
